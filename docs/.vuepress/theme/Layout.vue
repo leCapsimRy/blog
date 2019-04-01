dd778e8610c89427d5ef6771ac92f86817f508ac
wangzhichao <marsak@live.cn>
2019-04-01 17:16:16 +0800

    首页数据配置
    首页数据配置

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,17 +1,31 @@
 <template>
   <div class="theme-container">
+    <Loading v-if="display"></Loading>
     <Header></Header>
     <Container></Container >
     <!-- <Content/> -->
   </div>
 </template>
 <script>
+import Loading from './components/loading'
 import Header from './layouts/header'
 import Container from './layouts/container'
 export default {
+  data() {
+    return {
+      display:true
+    }
+  },
   components:{
+    Loading,
     Header,
     Container
+  },
+  created() {
+    this.display=true;
+  },
+  mounted() {
+    this.display=false;
   }
 }
 </script>
