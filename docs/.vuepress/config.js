7fa32ac11172547ab66358257e2b18081b01035d
wangzhichao <marsak@live.cn>
2019-04-12 14:47:28 +0800

    绑定域名

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: '杯酒故事 - 邵十一的博客', // 设置网站标题
     description: '我有故事，你有酒么？', //描述
     dest: './dist',   // 设置输出目录
-    base:'/blog/',
+    // base:'/blog/',
     port: 2233, //端口
     head: [
       ['link', { rel: 'shortcut icon', type: "image/x-icon", href: "./favicon.ico" }]
